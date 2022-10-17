7da5f8211c4535293b1d85d40e3492546f2682ed
iagosaito <iagosaito1999@gmail.com>
2022-10-17 18:40:04 -0300

    add lissajous

---
+++ a-linguagem-de-programacao-go/cap1/1_4.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	files := os.Args[1:]
+
+	if len(files) == 0 {
+		readFromStdin()
+	} else {
+		for _, filename := range files {
+			readFromFile(filename)
+		}
+	}
+}
+
+func readFromStdin() {
+	countLines(os.Stdin)
+}
+
+func readFromFile(filename string) {
+	file, err := os.Open(filename)
+	defer file.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	countLines(file)
+}
+
+func countLines(f *os.File) {
+	counts := make(map[string]int)
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		counts[scanner.Text()]++
+	}
+
+	for k, v := range counts {
+		fmt.Printf("%s -- %d\t%s\n", f.Name(), v, k)
+	}
+}
